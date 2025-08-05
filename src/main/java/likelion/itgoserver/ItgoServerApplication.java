7e737c8c7933eeaed3640918cfdab004b39a6fa8
woogie01 <kwonhu3973@gmail.com>
2025-08-05 10:38:08 +0900

    Initial commit

---
+++ src/main/java/likelion/itgoserver/ItgoServerApplication.java
@@ -0,0 +1,13 @@
+package likelion.itgoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItgoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItgoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/itgoserver/ItgoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.itgoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItgoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
